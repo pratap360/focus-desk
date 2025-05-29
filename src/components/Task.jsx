6233e628d4e37a87c4e6000b09ac5e1032321382
pratap360 <pratapparui0360@gmail.com>
2025-05-29 22:13:49 +0530

    feat: Update Task component with sound effects and improve UI styling

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,47 +1,56 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import NavBar from "./NavBar";
 import "./styles/task.css";
 import uselocalStorage from "../hooks/uselocalStorage";
 
 export default function Task() {
 
-  const [tasks,setTask] = uselocalStorage("todos",[
-    {id:1,text:"",done:false},
-    {id:2,text:"",done:false},
-    {id:3,text:"",done:false},
-  ])
+  const addTaskSfx = useRef(new Audio("../src/assets/sfx/SFX- Ui04.mp3"))
+  const deleteTaskSfx = useRef(new Audio("../src/assets/sfx/SFX- Ui09.mp3"))
+  const doneSfx = useRef(new Audio("../src/assets/sfx/SFX- Ui02.mp3"))
 
-  // const [tasks, setTasks] = useState([
-  //   { id: 1, text: "", completed: false },
-  //   { id: 2, text: "", completed: false },
-  //   { id: 3, text: "", completed: false },
-  // ]);
+  const [tasks, setTask] = uselocalStorage("todos", [
+    { id: 1, text: "", done: false , isDefault:true},
+    { id: 2, text: "", done: false , isDefault:false},
+    { id: 3, text: "", done: false , isDefault:false},
+  ]);
 
-  const handleTaskChange = (id,newText) => {
-    setTask(tasks.map(task =>
-  task.id === id? {...task,text:newText}: task))
-  }
+  const handleTaskChange = (id, newText) => {
+    setTask(
+      tasks.map(task => task.id === id ? { ...task, text: newText } : task)
+    );
+  };
 
   const toggleComplete = (id) => {
-    setTask(tasks.map(task => task.id === id ? {...task, done:!task.done }: task));
+    setTask(
+      tasks.map(task =>
+        task.id === id ? { ...task, done: !task.done } : task
+      )
+    );
+    doneSfx.current.play()
+    doneSfx.current.volume=0.7
   };
 
   const deleteTask = (id) => {
-    setTask(tasks.filter(task => task.id !==id))
+    const taskToDelete = tasks.find(task => task.id ===id)
+    if(taskToDelete.isDefault) return
+    setTask(tasks.filter(task => task.id !== id));
+    deleteTaskSfx.current.play()
+    deleteTaskSfx.current.volume = 0.5
   };
 
-
   const addMoreTask = () => {
+    if (tasks.length >= 7) return
     console.log("added More taks");
     const newTask = {
       id: Date.now(),
       text: "",
-      done: false
+      done: false,
+      isDefault: false
     }
-    // const newTask = document.createElement('input')
-    // newTask.className = 'task-checkbox'
-
-    setTask(prevTask => [...prevTask,newTask])
+    setTask([...tasks, newTask]);
+    addTaskSfx.current.play()
+    addTaskSfx.current.volume = 0.7
   };
 
   return (
@@ -49,34 +58,45 @@ export default function Task() {
       <NavBar />
       <div className="title-container">
         <h3>TODO's</h3>
-        <p>Top 3 Most Important Tasks of the Day</p>
+        <p>Top {tasks.length} Most Important Tasks of the Day</p>
       </div>
 
       <div className="task-container">
         <div className="task-list">
-          {tasks.map((task,index) => {
+          {tasks.map((task, index) => (
             <div key={task.id} className="task-item">
               <input
-              type="checkbox"
-              className="task-checkbox"
-              checked={task.done}
-              onChange={() => toggleComplete(task.id)}
+                type="checkbox"
+                className="task-checkbox"
+                checked={task.done}
+                onChange={() => toggleComplete(task.id)}
               />
 
               <input
-              type="text"
-              className="task-input"
-              placeholder={`Task ${index + 1}`}
-              value={task.text}
-              onChange={(e) => handleTaskChange(task.id, e.target.value)}
+                type="text"
+                className={`task-input ${task.done ? 'strikethrough' : ''}`}
+                placeholder={`Task ${index + 1}`}
+                value={task.text}
+                onChange={(e) => handleTaskChange(task.id, e.target.value)}
               />
-              <button className="delete-btn" onClick={() => {deleteTask(task.id)}}>
+              {!task.isDefault && (
+                <button
+                className="delete-btn"
+                onClick={() => {
+                  deleteTask(task.id);
+                }}
+              >
                 X
               </button>
+              )}
             </div>
-          })}
+          ))}
         </div>
-        <button className="add-task-btn" onClick={addMoreTask}>Add More Task</button>
+        {tasks.length <7 && (
+          <button className="add-task-btn" onClick={addMoreTask}>
+          Add More Task
+        </button>
+        )}
       </div>
     </>
   );
