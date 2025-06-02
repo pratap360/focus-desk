5217408da392459298c46286e2c9a19f8105080d
pratap360 <pratapparui0360@gmail.com>
2025-06-02 22:27:57 +0530

    feat: Enhance Stats component with quote fetching and improved styling

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,17 +1,56 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import NavBar from "./NavBar";
 import "./styles/stats.css";
+import uselocalStorage from "../hooks/uselocalStorage";
 
 export default function Stats() {
+
+  const [quote,setQuote] = useState({content:'',author:''})
+
+  // const [userData] = uselocalStorage("userData",{})
+  useEffect(() => {
+    const fetchQuote = async() => {
+      try {
+      const response = await fetch("https://api.quotable.io/random")
+      const data = await response.json()     
+      setQuote({
+        content:data.content,
+        author:data.authorSlug
+      })
+      } catch (error) {
+        console.error("Not fetching the quote url",error);
+        setQuote({
+          content:"Talk is cheap Show me your Code",
+          author:"Linus Torvalds"
+        })
+      }
+      
+    }
+    fetchQuote()
+  },[])
+
+
+  const data = localStorage.getItem("userData")
+  const res = JSON.parse(data)
+  console.log("User Name Data:", res.username);
+  
+
   return (
     <>
       <NavBar />
       <div className="stats-tittle">
         {/* <h3>{userData.username}'s FocusDesk Stats</h3> */}
-        <h3>Pratap's FocusDesk Stats</h3>
+        <h3>{res.username}'s FocusDesk Stats</h3>
         <hr />
       </div>
+
+      <div className="quote-container">
+        <p>{quote.content}</p>
+        <p> - {quote.author}</p>
+      </div>
+
     <div className="stats-container">
+
       <div className="focus-stats">
         <p>You have Focused for this 'time'</p>
           <hr />
